f24e5924bece0657c10d1e771f43e4e0040509a7
wuodland <karan_baruah@yahoo.co.in>
2019-04-15 17:59:27 +0700

    Main screen fix for FAB and basic layout

--- app/src/main/java/com/example/project_andclass2019/Tab1Fragment.java
+++ app/src/main/java/com/example/project_andclass2019/Tab1Fragment.java
@@ -16,14 +16,14 @@ public class Tab1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.tab1_fragment,container,false);
-        btnTEST = (Button) view.findViewById(R.id.btnTEST);
+       // btnTEST = (Button) view.findViewById(R.id.btnTEST);
 
-        btnTEST.setOnClickListener(new View.OnClickListener() {
+       /* btnTEST.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(getActivity(), "TESTING BUTTON CLICK 1",Toast.LENGTH_SHORT).show();
             }
-        });
+        }); */
 
         return view;
     }
