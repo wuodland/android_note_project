dcaed64a74b6dfbbd256cf15f6a4fdec5677b8b4
wuodland <karan_baruah@yahoo.co.in>
2019-04-17 03:39:39 +0700

    working state

--- app/src/main/java/com/example/project_andclass2019/db/NotesDB.java
+++ app/src/main/java/com/example/project_andclass2019/db/NotesDB.java
@@ -6,9 +6,7 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.content.Context;
 
-
 @Database(entities = Note.class, version = 1)
-
 public abstract class NotesDB extends RoomDatabase {
     public abstract NotesDao notesDao();
 
@@ -18,8 +16,7 @@ public abstract class NotesDB extends RoomDatabase {
     public static NotesDB getInstance(Context context) {
         if (instance == null)
             instance = Room.databaseBuilder(context, NotesDB.class, DATABSE_NAME)
-                    .allowMainThreadQueries()
-                    .build();
+                    .allowMainThreadQueries().build();
         return instance;
     }
 
